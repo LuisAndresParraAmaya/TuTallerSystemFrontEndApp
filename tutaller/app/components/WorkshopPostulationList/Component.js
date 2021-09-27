5e5f447bd3371d91889e5e892a381425239d906a
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-09-27 00:51:58 -0300

    HTTP fetch de listado de talleres en postulación

--- tutaller/app/components/WorkshopPostulationList/Component.js
+++ tutaller/app/components/WorkshopPostulationList/Component.js
@@ -1,8 +1,41 @@
 export default {
     name: 'WorkshopPostulationList',
+    data() {
+        return {
+          workshopPostulationList: ''
+        }
+    },
+
     methods: {
-        showWorkshopPostulation() {
-            this.$navigator.navigate('/ShowWorkshop')
+        showWorkshopPostulation(event) {
+            this.$navigator.navigate('/WorkshopPostulation', {props: {workshopPostulation: event.item}})  
+        },
+
+        getWorkshopPostulationList(){
+            fetch('http://10.0.2.2:8080/WorkshopPostulations', {
+                method: 'GET',
+                headers:{
+                  'Content-Type': 'application/json'
+                },
+              }).then(res => res.json())
+              .catch(error => {
+                console.error('Error:', error)
+                alert({
+                  title: 'Error',
+                  message: 'No se pudo realizar la acción. Comprueba la red e inténtalo de nuevo.',
+                  okButtonText: 'OK'
+                }).then(() => {
+                })
+              })
+              .then(response => {
+                switch (response.Response) {
+                    case 'Operation Success':
+                      this.workshopPostulationList = response.Postulations
+                      break
+                    case 'Operation Failed':
+                      console.log('fail')
+                }
+              })
         },
 
         goToPreviousPage(){
