51bf4c422a4f9d51a5ae126727b6f1f7e40c4db5
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-09-27 00:51:40 -0300

    Detalle postulación de taller (50%)

---
+++ tutaller/app/components/WorkshopPostulation/Template.vue
@@ -0,0 +1,18 @@
+<template>
+  <Page>
+      <ActionBar :title='workshopPostulation.workshop_name'>
+        <NavigationButton @tap='goToPreviousPage' android.systemIcon='@drawable/ic_menu_back'/>
+      </ActionBar>
+      <ScrollView>
+        <StackLayout>
+            <Label :text="workshopPostulation.postulation_message" textWrap="true"/>
+            <Label :text="workshopPostulation.postulation_date_time" textWrap="true"/>
+            <MDButton text="Rechazar" @tap="rejectWorkshopPostulation" class='danger'/>
+            <MDButton text="Aceptar" @tap="acceptWorkshopPostulation" />
+        </StackLayout>
+      </ScrollView>
+  </Page>
+</template>
+
+<script src='./Component.js'></script>
+<style src='./Style.css'></style>
