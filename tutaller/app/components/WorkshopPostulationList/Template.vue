d805e23f5f023dc54181e2a7b86bf358aeacb586
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-09-27 00:52:21 -0300

    ListView talleres postulación

--- tutaller/app/components/WorkshopPostulationList/Template.vue
+++ tutaller/app/components/WorkshopPostulationList/Template.vue
@@ -1,9 +1,17 @@
 <template>
-  <Page>
-        <ActionBar title='Postulaciones de Talleres'/>
-        <StackLayout>
-            
-        </StackLayout>
+  <Page @loaded='getWorkshopPostulationList'>
+        <ActionBar title='Postulaciones de Talleres'>
+          <NavigationButton @tap='goToPreviousPage' android.systemIcon='@drawable/ic_menu_back'/>
+        </ActionBar>
+        <ListView for="workshopPostulation in workshopPostulationList" @itemTap="showWorkshopPostulation">
+          <v-template>
+            <GridLayout columns='60%, *' rows='*, *'>
+              <Image src="res://outline_car_repair_black_36" stretch="aspectFit" row='0' col='0' rowSpan='2'/>
+              <Label :text="workshopPostulation.workshop_name" class='title-text' row='0' col='1'/>
+              <Label :text="workshopPostulation.postulation_date_time" class='caption-text' row='1' col='1'/>
+            </GridLayout>
+          </v-template>
+        </ListView>
   </Page>
 </template>
 
