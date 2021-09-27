aab53da4c2996232ab1bd35216fd05b5de6c09ae
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-09-27 00:50:39 -0300

    Gestión de talleres con botones para navegar

---
+++ tutaller/app/components/WorkshopManagement/Template.vue
@@ -0,0 +1,16 @@
+<template>
+  <Page>
+        <ActionBar title="Gestión de talleres">
+          <NavigationButton @tap='goToPreviousPage' android.systemIcon='@drawable/ic_menu_back'/>
+        </ActionBar>
+        <ScrollView>
+          <StackLayout>
+            <MDButton text="Postular taller" @tap="goToPostulateWorkshopPage"/>
+            <MDButton text="Listar talleres postulados" @tap="goToWorkshopPostulationListPage"/>
+          </StackLayout>
+        </ScrollView>
+  </Page>
+</template>
+
+<script src='./Component.js'></script>
+<style src='./Style.css'></style>
