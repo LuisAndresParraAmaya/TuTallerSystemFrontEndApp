32d62c4ea05278797ecb6e4fcf1ea38ec7b1a58b
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-09-27 00:49:23 -0300

    Eliminación de campos de sucursal en postulación de taller

--- tutaller/app/components/PostulateWorkshop/Template.vue
+++ tutaller/app/components/PostulateWorkshop/Template.vue
@@ -8,11 +8,8 @@
         <MDTextField v-model='workshopNameInput' hint='Nombre del taller' :error='workshopNameInputErr' @textChange='onWorkshopNameTxtChange'/>
         <MDTextField v-model='workshopPhoneInput' keyboardInput='phone' hint='Número de teléfono del taller' :error='workshopPhoneInputErr' @textChange='onWorkshopPhoneTxtChange'/>
         <MDTextField v-model='workshopDescriptionInput' hint='Descripción del taller' :error='workshopDescriptionInputErr' @textChange='onWorkshopDescriptionTxtChange'/>
-        <MDTextField v-model='workshopOfficeCommuneInput' hint='Comuna de la sucursal del taller' :error='workshopOfficeCommuneInputErr' @textChange='onWorkshopOfficeCommuneTxtChange'/>
-        <MDTextField v-model='workshopOfficeAddressInput' hint='Dirección de la sucursal taller' :error='workshopOfficeAddressInputErr' @textChange='onWorkshopOfficeAddressTxtChange'/>
-        <MDTextField v-model='workshopOfficePhoneInput' keyboardInput='phone' hint='Número de teléfono de la sucursal del taller' :error='workshopOfficePhoneInputErr' @textChange='onWorkshopOfficePhoneTxtChange'/>
         <MDTextView v-model='workshopPostulationMessage' hint='Mensaje' :error='workshopPostulationMessageErr' @textChange='onWorkshopPostulationMessageTxtChange'/>
-        <MDButton text='Enviar' @tap='sendPostulation' :isEnabled='isSendBtnTappable'/>
+        <MDButton text='Enviar postulación' @tap='sendPostulation' :isEnabled='isSendBtnTappable'/>
       </StackLayout>
     </ScrollView>
   </Page>
