f08f24d2cd0735cf0591cd50bc070b1231be53b9
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 22:46:01 -0300

    Options to show the workshop office subscription status and another to go to the offers in its services

--- tutaller/app/components/MyWorkshopOffice/Component.js
+++ tutaller/app/components/MyWorkshopOffice/Component.js
@@ -16,10 +16,13 @@ export default {
             this.$navigator.navigate('/WorkshopOfficeEmployeeList', { props: { workshopOfficeId: this.myWorkshopOffice.workshop_office_id, actualFrame: 'accountNav' }, frame: 'accountNav' })
         },
         goToMyWorkshopOfficeSubscriptionList() {
-            alert('Función en construcción')
+            this.$navigator.navigate('/MyWorkshopOfficeSubscription', { props: { myWorkshopOffice: this.myWorkshopOffice }, frame: 'accountNav' })
         },
         goToMyWorkshopOfficeAdList() {
             this.$navigator.navigate('/MyWorkshopOfficeAdList', { props: { workshopOfficeId: this.myWorkshopOffice.workshop_office_id }, frame: 'accountNav' })
+        },
+        goToOfferListPage() {
+            this.$navigator.navigate('/OfferList', { props: { workshopOfficeId: this.myWorkshopOffice.workshop_office_id, offerType: 'workshopOfficeService' }, frame: 'accountNav' })
         },
         goToPreviousPage() {
             this.$navigateBack()
