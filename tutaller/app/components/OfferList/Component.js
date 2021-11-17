2853ade476928d0ecea571c04996c9cad407ffc8
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 22:44:42 -0300

    Offer list logic

---
+++ tutaller/app/components/OfferList/Component.js
@@ -0,0 +1,116 @@
+import { translateSubscriptionName } from "~/utils/translators"
+import { formatDate } from "~/utils/formatter"
+const sqlite = require('nativescript-sqlite')
+
+export default {
+    props: ['offerType', 'workshopOfficeId'],
+    data() {
+        return {
+            offerList: [],
+            titleType: '',
+            translateSubscriptionName: translateSubscriptionName,
+            formatDate: formatDate
+        }
+    },
+
+    methods: {
+        showOffer(event) {
+            this.$navigator.navigate('/Offer', { props: { offer: event.item, offerType: this.offerType }, frame: 'accountNav', backstackVisible: false })
+        },
+
+        getOfferList() {
+            //Empty the offer list first
+            this.offerList = []
+            //Change the GUI title and get the offer list depending on the requested item (subscription plan or workshop office service)
+            if (this.offerType == 'subscriptionPlan') {
+                this.titleType = 'planes'
+                this.getSubscriptionList()
+            }
+            else if (this.offerType == 'workshopOfficeService') {
+                this.titleType = 'servicios'
+                this.getWorkshopOfficeServiceList()
+            }
+        },
+
+        getSubscriptionList() {
+            fetch('http://10.0.2.2:8080/SubscriptionList', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => res.json())
+                .catch(error => {
+                    console.error('Error:', error)
+                    alert({
+                        title: 'Error',
+                        message: 'No se pudo realizar la acción. Comprueba la red e inténtalo de nuevo.',
+                        okButtonText: 'OK'
+                    })
+                })
+                .then(response => {
+                    switch (response.Response) {
+                        case 'Operation Success':
+                            this.addOfferItemListTemp(response.SubscriptionList)
+                            break
+                        case 'Operation Failed':
+                            console.log('Subscriptions not found')
+                    }
+                })
+        },
+
+        getWorkshopOfficeServiceList() {
+            const data = { workshop_office_id: this.workshopOfficeId }
+            fetch('http://10.0.2.2:8080/WorkshopOfficeServiceList', {
+                method: 'POST',
+                body: JSON.stringify({ data }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => res.json())
+                .catch(error => {
+                    console.error('Error:', error)
+                    alert({
+                        title: 'Error',
+                        message: 'No se pudo realizar la acción. Comprueba la red e inténtalo de nuevo.',
+                        okButtonText: 'OK'
+                    })
+                })
+                .then(response => {
+                    switch (response.Response) {
+                        default:
+                            this.addOfferItemListTemp(response.response)
+                            break
+                        case 'Services not found':
+                            console.log('Services not found')
+                    }
+                })
+        },
+
+        addOfferItemListTemp(itemList) {
+            new sqlite('tutaller.db', (err, db) => {
+                db.execSQL('DROP TABLE IF EXISTS offer_item;')
+                db.execSQL('CREATE TABLE offer_item (id INTEGER, offer_name TEXT NOT NULL, offer_discount INTEGER NOT NULL, offer_valid_until_date TEXT NOT NULL, offer_valid_until_time TEXT NOT NULL, offer_item_name TEXT NOT NULL);')
+                itemList.forEach(element => {
+                    let addOfferItemValues = [element.offer_id, element.offer_name, element.offer_discount, element.offer_valid_until_date, element.offer_valid_until_time]
+                    if (this.offerType == 'subscriptionPlan') addOfferItemValues.push(element.name)
+                    else if (this.offerType == 'workshopOfficeService') addOfferItemValues.push(element.workshop_office_service_name)
+                    db.execSQL('INSERT INTO offer_item (id, offer_name, offer_discount, offer_valid_until_date, offer_valid_until_time, offer_item_name) VALUES (?, ?, ?, ?, ?, ?)', addOfferItemValues)
+                })
+                let promise = ''
+                promise = db.all('SELECT id, offer_name, offer_discount, offer_valid_until_date, offer_valid_until_time FROM offer_item WHERE offer_discount NOT IN (0) GROUP BY id;')
+                promise.then((resultSet) => {
+                    for (let i = 0; i < resultSet.length; i++) {
+                        this.offerList.push({ id: resultSet[i][0], offer_name: resultSet[i][1], offer_discount: resultSet[i][2], offer_valid_until_date: resultSet[i][3], offer_valid_until_time: resultSet[i][4] })
+                    }
+                })
+            })
+        },
+
+        goToAddOfferPage() {
+            this.$navigator.navigate('/AddOffer', { props: { offerType: this.offerType, workshopOfficeId: this.workshopOfficeId }, frame: 'accountNav', backstackVisible: false })
+        },
+        goToPreviousPage() {
+            this.$navigateBack()
+        }
+    }
+}
