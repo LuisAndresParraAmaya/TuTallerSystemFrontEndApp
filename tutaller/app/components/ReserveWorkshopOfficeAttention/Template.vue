f8c17d089c1609ee3bab08c6f575ba4cd005276b
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 23:54:58 -0300

    (demo) reserve workshop office attention logic

--- tutaller/app/components/ReserveWorkshopOfficeAttention/Template.vue
+++ tutaller/app/components/ReserveWorkshopOfficeAttention/Template.vue
@@ -1,12 +1,28 @@
 <template>
   <Page>
     <ActionBar title="Reservar atención">
-      <NavigationButton @tap='goToPreviousPage' android.systemIcon='@drawable/ic_menu_back'/>
+      <NavigationButton
+        @tap="goToPreviousPage"
+        android.systemIcon="@drawable/ic_menu_back"
+      />
     </ActionBar>
     <ScrollView>
       <StackLayout>
-          <DatePickerField hintDate="Fecha"/>
-          <TimePickerField hintTime="Hora"/>
+        <Label
+          text="Horario de atención"
+          textWrap="true"
+          class="paragraph font-bold"
+        />
+        <DatePickerField hint="Fecha de reserva" :minDate="new Date()" @dateChange="onReservedDateChange" />
+        <TimePickerField
+          hint="Hora de reserva"
+          @timeChange="onReservedTimeChange"
+        />
+        <Label
+          :text="reservedDatetimeErr"
+          textWrap="true"
+          class="regular-text error-color"
+        />
         <MDButton text="Reservar" @tap="reserveAttention" />
       </StackLayout>
     </ScrollView>
