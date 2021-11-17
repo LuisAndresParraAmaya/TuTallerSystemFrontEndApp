524de876af312e2041bade06b8b5f3c405ce7407
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 22:49:48 -0300

    Implement the CheckBox to allow its use in the app

--- tutaller/app/app.js
+++ tutaller/app/app.js
@@ -8,6 +8,7 @@ import { VueMaskFilter } from 'v-mask'
 import ButtonPlugin from '@nativescript-community/ui-material-button/vue'
 import FloatingActionButtonPlugin from '@nativescript-community/ui-material-floatingactionbutton/vue'
 import DateTimePicker from '@nativescript/datetimepicker/vue'
+import { CheckBox } from '@nstudio/nativescript-checkbox'
 
 Vue.use(Navigator, { routes })
 Vue.use(TextFieldPlugin)
@@ -16,5 +17,15 @@ Vue.filter('VMask', VueMaskFilter)
 Vue.use(ButtonPlugin)
 Vue.use(FloatingActionButtonPlugin)
 Vue.use(DateTimePicker)
+Vue.registerElement(
+    'CheckBox',
+    () => CheckBox,
+    {
+        model: {
+            prop: 'checked',
+            event: 'checkedChange'
+        }
+    }
+)
 
 new Vue({ render: h => h('frame', App), render: h => h(App), }).$start()
