06056d802c25f606c6142d9bcdae28cf337df6b2
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 22:39:13 -0300

    Functions to translate periodicity, postulation status and ad status

--- tutaller/app/utils/translators.js
+++ tutaller/app/utils/translators.js
@@ -8,3 +8,42 @@ export function translateSubscriptionName(suscriptionName) {
     }
     return suscriptionName
 }
+
+//Returns the translated periodicity
+export function translatePeriodicity(periodicity) {
+    switch (periodicity) {
+        case 'none':
+            return 'Ninguno'
+        case 'daily':
+            return 'Diario'
+        case 'monthly':
+            return 'Mensual'
+        case 'yearly':
+            return 'Anual'
+    }
+    return null
+}
+
+//Returns the translated postulation status
+export function translatePostulationStatus(status) {
+    switch (status) {
+        case 'pending':
+            return 'Pendiente'
+        case 'accepted':
+            return 'Aceptado'
+        case 'rejected':
+            return 'Rechazado'
+    }
+    return null
+}
+
+//Returns the translated ad status
+export function translateAdStatus(status) {
+    switch (status) {
+        case 'active':
+            return 'Activo'
+        case 'inactive':
+            return 'Inactivo'
+    }
+    return null
+}
