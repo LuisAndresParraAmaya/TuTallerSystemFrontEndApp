319fb1a1f732ec6031664afc409cea4284d6988a
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 22:42:08 -0300

    Subscription management template

---
+++ tutaller/app/components/SubscriptionManagement/Template.vue
@@ -0,0 +1,25 @@
+<template>
+  <Page>
+    <ActionBar title="Gestionar suscripciones" @loaded="onPageLoaded">
+      <NavigationButton
+        @tap="goToPreviousPage"
+        android.systemIcon="@drawable/ic_menu_back"
+      />
+    </ActionBar>
+    <ScrollView>
+      <StackLayout>
+        <MDButton
+          v-if="
+            userType == 1 //1 = admin
+          "
+          text="Ofertas"
+          @tap="goToOfferListPage"
+        />
+        <template v-else>
+        </template>
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script src='./Component.js'></script>
