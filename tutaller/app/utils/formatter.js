e3548b9a7b445cd6020e239bdd74be70ead213b0
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 22:39:55 -0300

    Changed the position of functions that were to translate instead

--- tutaller/app/utils/formatter.js
+++ tutaller/app/utils/formatter.js
@@ -11,6 +11,25 @@ export function formatDateTime(datetime) {
     return day + '/' + month + '/' + year + ' ' + hourminute
 }
 
+//Returns the date in the DD/MM/YYYY format
+export function formatDate(date) {
+    let d = date.split('T', 1)[0]
+    const year = d.split('-', 1)[0]
+    const month = d.split('-', 2)[1]
+    const day = d.split('-', 3)[2]
+    return day + '/' + month + '/' + year
+}
+
+//Formats the date to the format required in the DB (YYYY-MM-DD)
+export function formatDateToDB(date) {
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+}
+
+//Formats the time to the format required in the DB (hh:mm)
+export function formatTimeToDB(time) {
+    return time.getHours() + ':' + time.getMinutes()
+}
+
 //Returns the rating number, the rating number expressed in font awesome stars/half stars, and the number of total evaluations
 export function formatEvaluation(rating, totalEvaluations) {
     if (rating == 5) {
@@ -34,30 +53,6 @@ export function formatEvaluation(rating, totalEvaluations) {
     } else {
         return 'Sin valorizaciones'
     }
-}
-
-//Returns the translated postulation status
-export function formatPostulationStatus(status) {
-    switch (status) {
-        case 'pending':
-            return 'Pendiente'
-        case 'accepted':
-            return 'Aceptado'
-        case 'rejected':
-            return 'Rechazado'
-    }
-    return null
-}
-
-//Returns the translated ad status
-export function formatAdStatus(status) {
-    switch (status) {
-        case 'active':
-            return 'Activo'
-        case 'inactive':
-            return 'Inactivo'
-    }
-    return null
 }
 
 //Add the hyphen "-" to the Rut
