834f10fbfd280ee70dfefc39004d5528174a1ab0
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-16 23:55:35 -0300

    (demo) reserve workshop office attention logic. it allows the option to go to the pay service

--- tutaller/app/components/ReserveWorkshopOfficeAttention/Component.js
+++ tutaller/app/components/ReserveWorkshopOfficeAttention/Component.js
@@ -1,17 +1,42 @@
+import { validateDatetime } from "~/utils/validator"
+
 export default {
     props: ['workshopOfficeService'],
     data() {
         return {
             reservedDateInput: '',
-            reservedTimeInput: ''
+            reservedTimeInput: '',
+            reservedDatetimeErr: ''
         }
     },
 
     methods: {
         reserveAttention() {
-            console.log(this.workshopOfficeService)
-            alert('Función en construcción')
-            //this.$navigator.navigate('/PayWorkshopOfficeService')
+            if (this.validateFormReserveAttention()) {
+                this.$navigator.navigate('/PayWorkshopOfficeService')
+            }
+        },
+
+        validateFormReserveAttention() {
+            let isValidationOK = true
+            //Valid until date/time validation
+            let reservedDatetimeValidationRes = validateDatetime(this.reservedDateInput, this.reservedTimeInput, new Date())
+            if (reservedDatetimeValidationRes !== null) {
+                this.reservedDatetimeErr = reservedDatetimeValidationRes
+                isValidationOK = false
+            }
+            //Check if validation is OK
+            if (isValidationOK) return true
+            else return false
+        },
+
+        onReservedDateChange(event) {
+            this.reservedDateInput = event.value
+            this.reservedDatetimeErr = ''
+        },
+        onReservedTimeChange(event) {
+            this.reservedTimeInput = event.value
+            this.reservedDatetimeErr = ''
         },
 
         goToPreviousPage() {
