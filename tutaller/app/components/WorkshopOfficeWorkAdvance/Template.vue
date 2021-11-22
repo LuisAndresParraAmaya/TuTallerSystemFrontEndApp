bddef6fb3f37e103ef595360ef7ffd5395cc8f63
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-22 10:19:16 -0300

    Workshop office work advance, in demo state

---
+++ tutaller/app/components/WorkshopOfficeWorkAdvance/Template.vue
@@ -0,0 +1,22 @@
+<template>
+  <Page>
+    <ActionBar title="Avance">
+      <NavigationButton
+        @tap="goToPreviousPage"
+        android.systemIcon="@drawable/ic_menu_back"
+      />
+    </ActionBar>
+    <ScrollView>
+      <StackLayout>
+        <Image src="http://10.0.2.2:8080/img" stretch="aspectFit" />
+        <Label
+          :text="workshopOfficeWorkAdvance.workshop_office_service_advance_description"
+          textWrap="true"
+          class="paragraph"
+        />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script src='./Component.js'></script>
