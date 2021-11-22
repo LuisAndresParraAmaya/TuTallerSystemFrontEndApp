ee5b39ec609f2d8d4afdb7a0d86f2c7970aa073d
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-22 10:28:50 -0300

    Payment receipt view, allowing to see the general information from the receipt and button to continue

---
+++ tutaller/app/components/PaymentReceipt/Template.vue
@@ -0,0 +1,49 @@
+<template>
+  <Page>
+    <ActionBar title="Acuso de recibo">
+      <NavigationButton
+        @tap="goToPreviousPage"
+        android.systemIcon="@drawable/ic_menu_back"
+      />
+    </ActionBar>
+    <StackLayout>
+      <Label
+        text="Nombre del ítem"
+        textWrap="true"
+        class="paragraph font-bold"
+      />
+      <Label text="nombre" textWrap="true" class="paragraph" />
+      <Label text="Monto pagado" textWrap="true" class="paragraph font-bold" />
+      <Label text="precio" textWrap="true" class="paragraph" />
+      <Label
+        text="Método de pago"
+        textWrap="true"
+        class="paragraph font-bold"
+      />
+      <Label text="m.pago" textWrap="true" class="paragraph" />
+      <Label
+        text="Fecha y hora de pago"
+        textWrap="true"
+        class="paragraph font-bold"
+      />
+      <Label text="DD/MM/YYYY hh:mm" textWrap="true" class="paragraph" />
+      <Label
+        text="Para su seguridad, el pago será retenido hasta obtener una confirmación de que el servicio automotriz sea realizado en su totalidad por parte del técnico y por parte suya."
+        textWrap="true"
+        class="paragraph info-bg"
+      />
+      <MDButton
+        text="Descargar recibo"
+        @tap="downloadPaymentReceipt"
+        class="outline-btn"
+      />
+      <MDButton
+        text="Continuar"
+        @tap="continueProcess"
+        :isEnabled="isContinueBtnTappable"
+      />
+    </StackLayout>
+  </Page>
+</template>
+
+<script src='./Component.js'></script>
