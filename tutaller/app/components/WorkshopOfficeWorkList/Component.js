a285edf61c1819d7739c880287bf3336d8c286d7
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-22 10:19:01 -0300

    Workshop office work list logic, allowing to select any of the works in the list. Only applies if the user is related to any of them

---
+++ tutaller/app/components/WorkshopOfficeWorkList/Component.js
@@ -0,0 +1,50 @@
+import { ApplicationSettings } from "@nativescript/core"
+import { translateWorkStatus } from "~/utils/translators"
+
+export default {
+    data() {
+        return {
+            workshopOfficeWorkList: '',
+            translateWorkStatus: translateWorkStatus
+        }
+    },
+
+    methods: {
+        showWorkshopOfficeWork(event) {
+            this.$navigator.navigate('/WorkshopOfficeWork', { props: { workshopOfficeWork: event.item }, frame: 'serviceNav' })
+        },
+
+        getWorkshopOfficeWorkList() {
+            const data = { user_rut: ApplicationSettings.getString('user') }
+
+            fetch('http://10.0.2.2:8080/WorkshopOfficeWorkList', {
+                method: 'POST',
+                body: JSON.stringify({ data }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => res.json())
+                .catch(error => {
+                    console.error('Error:', error)
+                    alert({
+                        title: 'Error',
+                        message: 'No se pudo realizar la acción. Comprueba la red e inténtalo de nuevo.',
+                        okButtonText: 'OK'
+                    })
+                })
+                .then(response => {
+                    switch (response.Response) {
+                        case 'Operation Success':
+                            this.workshopOfficeWorkList = response.WorkshopOfficeWorkList
+                            break
+                        case 'Workshop office works not found':
+                            console.log('Workshop office works not found')
+                    }
+                })
+        },
+
+        goToPreviousPage() {
+            this.$navigateBack()
+        }
+    }
+}
