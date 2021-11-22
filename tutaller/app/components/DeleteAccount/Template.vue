4179d7c07b273b5a92bc446b6cd30aacce5abe15
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-22 10:33:21 -0300

    Css name made more clear (danger -> danger-color)

--- tutaller/app/components/DeleteAccount/Template.vue
+++ tutaller/app/components/DeleteAccount/Template.vue
@@ -7,7 +7,7 @@
       <StackLayout>
         <MDTextField v-model='actualPasswordInput' secure='true' hint='Contraseña actual' :error='actualPasswordInputErr' @textChange='onPasswordTxtChange'/>
         <MDTextField v-model='confirmActualPasswordInput' secure='true' hint='Confirmar contraseña actual' :error='confirmActualPasswordInputErr' @textChange='onPasswordTxtChange'/>
-        <MDButton text='Eliminar mi cuenta' @tap='deleteAccount' class='danger' :isEnabled='isDeleteAccountTappable'/>
+        <MDButton text='Eliminar mi cuenta' @tap='deleteAccount' class='danger-color' :isEnabled='isDeleteAccountTappable'/>
         <Label text="La eliminación de tu cuenta será realizada al transcurrir 30 días desde su desactivación. Para reactivar tu cuenta solo debes volver a iniciar sesión dentro de dicho periodo." textWrap="true" class="paragraph"/>
       </StackLayout>
     </ScrollView>
