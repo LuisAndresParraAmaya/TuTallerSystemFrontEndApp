94056b10bc787d6fb199ecca9c8b91a02734cd4f
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-11-22 22:55:12 -0300

    Estructure change (auto format)

--- tutaller/app/components/App.vue
+++ tutaller/app/components/App.vue
@@ -1,15 +1,27 @@
 <template>
-    <Page>
-        <TabView androidTabsPosition='bottom' tabBackgroundColor='@color/ns_primaryDark' selectedTabTextColor='white' tabTextColor='#a9a9a9' androidSelectedTabHighlightColor='@color/ns_primaryDark'>
-            <TabViewItem title='Talleres' iconSource='res://outline_car_repair_white_36'>
-                <Navigator defaultRoute='/WorkshopOfficeList'/>
-            </TabViewItem>
-            <TabViewItem title='Servicios' iconSource='res://outline_construction_white_36'>
-                <Navigator id='serviceNav' defaultRoute='/WorkshopOfficeWorkList'/>
-            </TabViewItem>
-            <TabViewItem title='Cuenta' iconSource='res://outline_person_white_36'>
-                <Navigator id='accountNav' defaultRoute='/AccountOptions'/>
-            </TabViewItem>
-        </TabView>
-    </Page>
+  <Page>
+    <TabView
+      androidTabsPosition="bottom"
+      tabBackgroundColor="@color/ns_primaryDark"
+      selectedTabTextColor="white"
+      tabTextColor="#a9a9a9"
+      androidSelectedTabHighlightColor="@color/ns_primaryDark"
+    >
+      <TabViewItem
+        title="Talleres"
+        iconSource="res://outline_car_repair_white_36"
+      >
+        <Navigator defaultRoute="/WorkshopOfficeList" />
+      </TabViewItem>
+      <TabViewItem
+        title="Servicios"
+        iconSource="res://outline_construction_white_36"
+      >
+        <Navigator id="serviceNav" defaultRoute="/WorkshopOfficeWorkList"/>
+      </TabViewItem>
+      <TabViewItem title="Cuenta" iconSource="res://outline_person_white_36">
+        <Navigator id="accountNav" defaultRoute="/AccountOptions" />
+      </TabViewItem>
+    </TabView>
+  </Page>
 </template>
