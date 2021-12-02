1208aa4e4f7dc1e46e330a9d8bfe16a9108eff03
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-12-01 21:23:43 -0300

    Reserve workshop office attention template, showing the office attention and the fields to choose a date and time

--- tutaller/app/components/ReserveWorkshopOfficeAttention/Template.vue
+++ tutaller/app/components/ReserveWorkshopOfficeAttention/Template.vue
@@ -8,12 +8,40 @@
     </ActionBar>
     <ScrollView>
       <StackLayout>
+        <Label text="Horas disponibles" textWrap="true" class="paragraph font-bold" />
+        <template
+          v-for="workshopOfficeAttention in workshopOfficeAttentionList"
+        >
+          <Label
+            :key="workshopOfficeAttention"
+            :text="
+              translateWeekDay(
+                workshopOfficeAttention.workshop_office_attention_day
+              ) +
+              ' ' +
+              formatTimeHM(
+                workshopOfficeAttention.workshop_office_attention_aperture_time
+              ) +
+              ' - ' +
+              formatTimeHM(
+                workshopOfficeAttention.workshop_office_attention_departure_time
+              )
+            "
+            textWrap="true"
+            class="paragraph"
+          >
+          </Label>
+        </template>
         <Label
-          text="Horario de atención"
+          text="Fecha y hora de reserva"
           textWrap="true"
           class="paragraph font-bold"
         />
-        <DatePickerField hint="Fecha de reserva" :minDate="new Date()" @dateChange="onReservedDateChange" />
+        <DatePickerField
+          hint="Fecha de reserva"
+          :minDate="new Date()"
+          @dateChange="onReservedDateChange"
+        />
         <TimePickerField
           hint="Hora de reserva"
           @timeChange="onReservedTimeChange"
