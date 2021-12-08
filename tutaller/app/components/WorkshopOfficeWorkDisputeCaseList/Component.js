fae51a42f05fa69c8a9350b0e9db2885bbfd2ccc
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-12-07 21:38:52 -0300

    Logic for the dispute case list, allowing to get the detail when clicking one case

---
+++ tutaller/app/components/WorkshopOfficeWorkDisputeCaseList/Component.js
@@ -0,0 +1,49 @@
+import { translateServiceWorkDisputeCaseStatus } from "~/utils/translators"
+import { formatDateTime } from "~/utils/formatter"
+
+export default {
+  data() {
+    return {
+      workshopOfficeWorkDisputeCaseList: '',
+      translateServiceWorkDisputeCaseStatus: translateServiceWorkDisputeCaseStatus,
+      formatDateTime: formatDateTime
+    }
+  },
+
+  methods: {
+    showWorkshopOfficeWorkDisputeCase(event) {
+      this.$navigator.navigate('/WorkshopOfficeWorkDisputeCase', { props: { workshopOfficeWorkDisputeCase: event.item }, frame: 'accountNav', backstackVisible: false })
+    },
+
+    getWorkshopOfficeWorkDisputeCaseList() {
+      fetch('http://10.0.2.2:8080/WorkshopOfficeWorkDisputeCaseList', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }).then(res => res.json())
+        .catch(error => {
+          console.error('Error:', error)
+          alert({
+            title: 'Error',
+            message: 'No se pudo realizar la acción. Comprueba la red e inténtalo de nuevo.',
+            okButtonText: 'OK'
+          }).then(() => {
+          })
+        })
+        .then(response => {
+          switch (response.Response) {
+            case 'Operation Success':
+              this.workshopOfficeWorkDisputeCaseList = response.WorkshopOfficeWorkDisputeCaseList
+              break
+            case 'Work disputes not found':
+              console.log('Work disputes not found')
+          }
+        })
+    },
+
+    goToPreviousPage() {
+      this.$navigateBack()
+    }
+  }
+}
