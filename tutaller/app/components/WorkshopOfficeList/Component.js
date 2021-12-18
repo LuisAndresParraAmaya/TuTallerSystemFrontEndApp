ab4be91b710616369dffd2052d017003fd51541b
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-12-18 17:35:05 -0300

    Added the show usability questionnaire to user feature inside the workshop office list

--- tutaller/app/components/WorkshopOfficeList/Component.js
+++ tutaller/app/components/WorkshopOfficeList/Component.js
@@ -1,4 +1,5 @@
 import { formatQualification } from "~/utils/formatter"
+import { ApplicationSettings } from "@nativescript/core"
 
 export default {
     props: ['filteredWorkshopOfficeList'],
@@ -11,6 +12,17 @@ export default {
     },
 
     methods: {
+        onPageLoaded(event) {
+            this.getWorkshopList()
+            //refresh ad image
+            event.object.getViewById('imgWorkshopOfficeAd').src = 'http://10.0.2.2:8080/img?t' + new Date().getTime()
+            //Temporal solution to make the service tab in bottom navigation to update the WorkshopOfficeWorkList view (TODO)
+            if (this.$navigator.paths.serviceNav == '/WorkshopOfficeWorkList') this.$navigator.navigate('/WorkshopOfficeWorkList', { frame: 'serviceNav' })
+            //Show usability questionnaire (if any is active), not showing it to the system admin
+            if (ApplicationSettings.getString('userType') !== '1' && ApplicationSettings.getString('user') !== undefined) this.showUsabilityQuestionnaire()
+        },
+
+        //Get the workshop office list, ordered by nearest location by geolocation and total rating
         getWorkshopList() {
             fetch('http://10.0.2.2:8080/WorkshopOfficeList', {
                 method: 'GET',
@@ -37,12 +49,31 @@ export default {
                 })
         },
 
-        onPageLoaded(event) {
-            this.getWorkshopList()
-            //refresh ad image
-            event.object.getViewById('imgWorkshopOfficeAd').src = 'http://10.0.2.2:8080/img?t' + new Date().getTime()
-            //Temporal solution to make the service tab in bottom navigation to update the WorkshopOfficeWorkList view (TODO)
-            if (this.$navigator.paths.serviceNav == '/WorkshopOfficeWorkList') this.$navigator.navigate('/WorkshopOfficeWorkList', { frame: 'serviceNav' })
+        showUsabilityQuestionnaire() {
+            const data = { user_user_rut: ApplicationSettings.getString('user') }
+
+            fetch('http://10.0.2.2:8080/ShowUsabilityQuestionnaire', {
+                method: 'POST',
+                body: JSON.stringify({ data }),
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(res => res.json())
+                .catch(error => {
+                    console.error('Error:', error)
+                })
+                .then(response => {
+                    switch (response.Response) {
+                        case 'Operation Success':
+                            this.$navigator.modal('/ShowUsabilityQuestionnaire', { props: { usabilityQuestionnaire: response.UsabilityQuestionnaire[0] }, id: 'modalShowUsabilityQuestionnaire', fullscreen: true })
+                            break
+                        case 'No active questionnaire found':
+                            console.log('No active questionnaire found')
+                            break
+                        case 'User cannot do the questionnaire':
+                            console.log('User cannot do the questionnaire')
+                    }
+                })
         },
 
         showWorkshopOffice(event) {
