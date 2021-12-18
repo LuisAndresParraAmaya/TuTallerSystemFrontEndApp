d762929c018e2cae9b23a10d4f90253857b5be75
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-12-18 17:37:19 -0300

    New show usability questionnaire template, that is a modal

---
+++ tutaller/app/components/ShowUsabilityQuestionnaire/Template.vue
@@ -0,0 +1,42 @@
+<template>
+  <Page>
+    <ActionBar title="Cuestionario">
+      <NavigationButton
+        @tap="closeModal"
+        android.systemIcon="@drawable/ic_menu_back"
+      />
+    </ActionBar>
+    <StackLayout>
+      <Label
+        :text="usabilityQuestionnaire.usability_questionnaire_name"
+        textWrap="true"
+        class="paragraph font-bold"
+      />
+      <Label
+        :text="usabilityQuestionnaire.usability_questionnaire_description"
+        textWrap="true"
+        class="paragraph"
+      />
+      <MDButton
+        text="Realizar"
+        class="text-btn"
+        @tap="goToUsabilityQuestionnairePage"
+        :isEnabled="isRealizeBtnTappable"
+      />
+      <MDButton
+        text="Hacer más tarde"
+        class="text-btn"
+        @tap="doUsabilityQuestionnaireLater"
+        :isEnabled="isDoLaterBtnTappable"
+      />
+      <MDButton
+        text="No volver a recordar"
+        class="text-btn"
+        @tap="stopRemindingUsabilityQuestionnaire"
+        :isEnabled="isStopRemindBtnTappable"
+      />
+    </StackLayout>
+  </Page>
+</template>
+
+<script src='./Component.js'></script>
