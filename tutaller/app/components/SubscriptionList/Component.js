efac4117b292cb62bbe8972fe17cab0d5c3af130
raulperez16 <90328029+raulperez16@users.noreply.github.com>
2021-12-18 17:36:57 -0300

    Removed alert that could pop anywhere when navigating the app. Also refactorized the code.

--- tutaller/app/components/SubscriptionList/Component.js
+++ tutaller/app/components/SubscriptionList/Component.js
@@ -2,48 +2,43 @@ import { translateSubscriptionName } from "~/utils/translators"
 import { translatePeriodicity } from "~/utils/translators"
 
 export default {
-    props: ['myWorkshopOffice'],
-    data() {
-        return {
-            subscriptionList: '',
-            translateSubscriptionName: translateSubscriptionName,
-            translatePeriodicity: translatePeriodicity
-        }
+  props: ['myWorkshopOffice'],
+  data() {
+    return {
+      subscriptionList: '',
+      translateSubscriptionName: translateSubscriptionName,
+      translatePeriodicity: translatePeriodicity
+    }
+  },
+
+  methods: {
+    showSubscription(event) {
+      this.$navigator.navigate('/Subscription', { props: { subscription: event.item, myWorkshopOffice: this.myWorkshopOffice }, frame: 'accountNav' })
     },
 
-    methods: {
-        showSubscription(event) {
-          this.$navigator.navigate('/Subscription', { props: { subscription: event.item, myWorkshopOffice: this.myWorkshopOffice }, frame: 'accountNav' })
+    getSubscriptionList() {
+      fetch('http://10.0.2.2:8080/SubscriptionList', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
         },
+      }).then(res => res.json())
+        .catch(error => {
+          console.error('Error:', error)
+        })
+        .then(response => {
+          switch (response.Response) {
+            case 'Operation Success':
+              this.subscriptionList = response.SubscriptionList
+              break
+            case 'Operation Failed':
+              console.log('Subscriptions not found')
+          }
+        })
+    },
 
-        getSubscriptionList() {
-            fetch('http://10.0.2.2:8080/SubscriptionList', {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-            }).then(res => res.json())
-              .catch(error => {
-                console.error('Error:', error)
-                alert({
-                  title: 'Error',
-                  message: 'No se pudo realizar la acción. Comprueba la red e inténtalo de nuevo.',
-                  okButtonText: 'OK'
-                })
-              })
-              .then(response => {
-                switch (response.Response) {
-                  case 'Operation Success':
-                    this.subscriptionList = response.SubscriptionList
-                    break
-                  case 'Operation Failed':
-                    console.log('Subscriptions not found')
-                }
-              })
-          },
-
-        goToPreviousPage() {
-            this.$navigateBack()
-        }
+    goToPreviousPage() {
+      this.$navigateBack()
     }
+  }
 }
